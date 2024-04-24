793e2f9f11cab161a752f2db1905bf7747b36f6d
Aleneitor-Void <aleth2k@gmail.com>
2024-04-24 09:38:47 -0600

    implents sunction sto use only one board

--- main.js
+++ main.js
@@ -9,16 +9,10 @@ const chessboard = [
   { a8: 2, b8: 1, c8: 2, d8: 1, e8: 2, f8: 1, g8: 2, h8: 1 },
 ];
 
-const chessboardReverse = [
-  { h1: 1, g1: 2, f1: 1, e1: 2, d1: 1, c1: 2, b1: 1, a1: 2 },
-  { h2: 2, g2: 1, f2: 2, e2: 1, d2: 2, c2: 1, b2: 2, a2: 1 },
-  { h3: 1, g3: 2, f3: 1, e3: 2, d3: 1, c3: 2, b3: 1, a3: 2 },
-  { h4: 2, g4: 1, f4: 2, e4: 1, d4: 2, c4: 1, b4: 2, a4: 1 },
-  { h5: 1, g5: 2, f5: 1, e5: 2, d5: 1, c5: 2, b5: 1, a5: 2 },
-  { h6: 2, g6: 1, f6: 2, e6: 1, d6: 2, c6: 1, b6: 2, a6: 1 },
-  { h7: 1, g7: 2, f7: 1, e7: 2, d7: 1, c7: 2, b7: 1, a7: 2 },
-  { h8: 2, g8: 1, f8: 2, e8: 1, d8: 2, c8: 1, b8: 2, a8: 1 },
-];
+const mainContainer = document.getElementById("main");
+
+let chessNotationColumns = ["a", "b", "c", "d", "e", "f", "g", "h"];
+let chessNotationRows = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 function reverseObject(object) {
   const keys = Object.keys(object);
@@ -29,27 +23,16 @@ function reverseObject(object) {
   return reverseObject;
 }
 
-function reverseBoard(board) {
-
+function reverseBoard(chessBoard) {
+  const board = chessBoard;
+  
+  for (const key in board) {
+    if (Object.hasOwnProperty.call(board, key)) {
+      board[key] = reverseObject(board[key]);
+    }
+  }
+  return board;
 }
-
-const objetoOriginal = {
-  a1: 1,
-  b1: 2,
-  c1: 1,
-  d1: 2,
-  e1: 1,
-  f1: 2,
-  g1: 1,
-  h1: 2,
-};
-const objetoInvertido = reverseObject(objetoOriginal);
-console.log(objetoInvertido);
-
-const mainContainer = document.getElementById("main");
-
-let chessNotationColumns = ["a", "b", "c", "d", "e", "f", "g", "h"];
-let chessNotationRows = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
 function createColumns(num) {
   const colContainer = document.createElement("div");
@@ -92,8 +75,8 @@ function createChessBoard() {
   return board;
 }
 
-function drawBoard(chessBoardWhite, chessBoardBlack, whiteBoard) {
-  const chessboard = whiteBoard ? chessBoardWhite.reverse() : chessBoardBlack;
+function drawBoard(chessBoardWhite, whiteBoard) {
+  const chessboard = whiteBoard ? chessBoardWhite.reverse() : reverseBoard(chessBoardWhite);
   const squareColor1 = whiteBoard ? "white" : "black";
   const squareColor2 = whiteBoard ? "black" : "white";
 
@@ -120,5 +103,5 @@ function drawBoard(chessBoardWhite, chessBoardBlack, whiteBoard) {
 }
 
 window.onload = () => {
-  drawBoard(chessboard, chessboardReverse, false);
+  drawBoard(chessboard, true);
 };
